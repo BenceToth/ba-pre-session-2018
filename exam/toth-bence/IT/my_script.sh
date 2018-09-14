90cc67a0294e0844d89e9ceb6be874580cfd620b
BenceToth <toth_bence02@student.ceu.edu>
2018-09-14 17:03:01 +0200

    Finalize code in my_script.sh

--- exam/toth-bence/IT/my_script.sh
+++ exam/toth-bence/IT/my_script.sh
@@ -1,4 +1,8 @@
 MKDIR made_by_my_script
 cd made_by_my_script
-echo 'Hello World' > hello.txt
+echo 'Hello world' > hello.txt
+git add hello.txt
+git commit -m 'add hello.txt to made_by_my_script subfolder'
 ls > file_list.txt
+git add file_list.txt
+git commit -m 'add file_list.txt to made_by_my_script subfolder'
